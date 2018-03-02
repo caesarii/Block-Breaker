2e4ecaf8ecf20f30769f965b556e1b9a96fb692f
caesarii <iwangqinghe@gmail.com>
2018-03-02 15:09:24 +0800

    create Image

---
+++ game/Image.js
@@ -0,0 +1,13 @@
+
+class Image extends GameObject {
+    constructor() {
+        super()
+        this.image = null
+        this.w = 0
+        this.h = 0
+        this.x = 0
+        this.y = 0
+        this.speedX = 0
+        this.speedY = 0
+    }
+}
