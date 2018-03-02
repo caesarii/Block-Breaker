ee3543f8294753e2ae94664e6d62a5d166a7b3b7
caesarii <iwangqinghe@gmail.com>
2018-03-02 15:10:38 +0800

    refactor Ball

--- scenes/main/ball.js
+++ scenes/main/ball.js
@@ -1,15 +1,24 @@
-const Ball = function(game) {
-    const o = game.imageByName('ball')
-    // const image = imageFromPath('ball.png')
-    o.x = 100
-    o.y = 200
-    o.speedX = 5
-    o.speedY = 5
-    o.fired = false
-    o.fire = function() {
-        o.fired = true
+class Ball extends Image {
+    constructor(game) {
+        super()
+        const img = game.imageByName('ball')
+        // Image 共有的属性
+        this.image = img
+        this.w = img.width
+        this.h = img.height
+        this.x = 100
+        this.y = 200
+        this.speedX = 5
+        this.speedY = 5
+        
+        // Ball 特有的属性
+        this.fired = false
     }
-    o.move = function() {
+    fire() {
+        this.fired = true
+    }
+    move() {
+        const o = this
         if (o.fired) {
             // log('move')
             if (o.x < 0 || o.x > 400) {
@@ -23,13 +32,50 @@ const Ball = function(game) {
             o.y += o.speedY
         }
     }
-    o.反弹 = function() {
-        o.speedY *= -1
+    bounce() {
+        this.speedY *= -1
     }
-    o.hasPoint = function(x, y) {
+    hasPoint(x, y) {
+        const o = this
         const xIn = x >= o.x && x <= o.x + o.w
         const yIn = y >= o.y && y <= o.y + o.h
         return xIn && yIn
     }
-    return o
 }
+//
+// const Ball = function(game) {
+//     const o = game.imageByName('ball')
+//     // const image = imageFromPath('ball.png')
+//     o.x = 100
+//     o.y = 200
+//     o.speedX = 5
+//     o.speedY = 5
+//     o.fired = false
+//
+//     o.fire = function() {
+//         o.fired = true
+//     }
+//     o.move = function() {
+//         if (o.fired) {
+//             // log('move')
+//             if (o.x < 0 || o.x > 400) {
+//                 o.speedX = -o.speedX
+//             }
+//             if (o.y < 0 || o.y > 300) {
+//                 o.speedY = -o.speedY
+//             }
+//             // move
+//             o.x += o.speedX
+//             o.y += o.speedY
+//         }
+//     }
+//     o.反弹 = function() {
+//         o.speedY *= -1
+//     }
+//     o.hasPoint = function(x, y) {
+//         const xIn = x >= o.x && x <= o.x + o.w
+//         const yIn = y >= o.y && y <= o.y + o.h
+//         return xIn && yIn
+//     }
+//     return o
+// }
