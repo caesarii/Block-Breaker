cd62eefa9cd704b4b42c8f513852330d5ba355a6
caesarii <iwangqinghe@gmail.com>
2018-03-02 15:37:23 +0800

    refactor Block

--- scenes/main/block.js
+++ scenes/main/block.js
@@ -1,25 +1,50 @@
-const Block = function(game, position) {
-    // positon 是 [0, 0] 格式
-    const p = position
-    const img = game.imageByName('block')
-    const o = {
-        x: p[0],
-        y: p[1],
-        alive: true,
-        lifes: p[2] || 1,
+class Block extends GameImage {
+    constructor(game, position) {
+        super(game, position)
+        const p = position
+        const img = game.imageByName('block')
+        this.x = p[0]
+        this.y = p[1]
+        this.alive = true
+        this.lifes = p[2] || 1
+        this.image = img
+        this.w = img.width
+        this.h = img.height
     }
-    o.image = img.image
-    o.w = img.w
-    o.h = img.h
-    o.kill = function() {
+    kill() {
         o.lifes--
         if (o.lifes < 1) {
             o.alive = false
         }
     }
-    o.collide = function(b) {
-        // log('block', o.alive, b)
+    collide() {
         return o.alive && (rectIntersects(o, b) || rectIntersects(b, o))
     }
-    return o
 }
+
+//
+// const Block = function(game, position) {
+//     // positon 是 [0, 0] 格式
+//     const p = position
+//     const img = game.imageByName('block')
+//     const o = {
+//         x: p[0],
+//         y: p[1],
+//         alive: true,
+//         lifes: p[2] || 1,
+//     }
+//     o.image = img.image
+//     o.w = img.w
+//     o.h = img.h
+//     o.kill = function() {
+//         o.lifes--
+//         if (o.lifes < 1) {
+//             o.alive = false
+//         }
+//     }
+//     o.collide = function(b) {
+//         // log('block', o.alive, b)
+//         return o.alive && (rectIntersects(o, b) || rectIntersects(b, o))
+//     }
+//     return o
+// }
