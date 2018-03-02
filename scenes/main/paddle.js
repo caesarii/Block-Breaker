a52167a5b8652b7a4c54c0a4f773fe2c620c248b
caesarii <iwangqinghe@gmail.com>
2018-03-02 15:53:50 +0800

    refactor Paddle

--- scenes/main/paddle.js
+++ scenes/main/paddle.js
@@ -1,16 +1,17 @@
-const Paddle = function(game) {
-    const o = game.imageByName('paddle')
-    // const o = {
-    //     image: image,
-    //     x: 100,
-    //     y: 250,
-    //     speed: 15,
-    // }
-    o.x = 100
-    o.y = 250
-    o.speed = 15
-    const paddle = o
-    o.move = function(x) {
+class Paddle extends GameImage {
+    constructor(game) {
+        super(game)
+        const img = game.imageByName('paddle')
+        this.image = img
+        this.w = img.width
+        this.h = img.height
+        this.x = 100
+        this.y = 250
+        this.speed = 15
+        
+    }
+    move(x) {
+        const o = this
         if (x < 0) {
             x = 0
         }
@@ -19,16 +20,19 @@ const Paddle = function(game) {
         }
         o.x = x
     }
-    o.moveLeft = function() {
-        o.move(paddle.x - paddle.speed)
+    
+    moveLeft() {
+        this.move(this.x - this.speed)
     }
-    o.moveRight = function() {
-        o.move(paddle.x + paddle.speed)
+    
+    moveRight() {
+        this.move(this.x + this.speed)
     }
-    const aInb = function(x, x1, x2) {
-        return x >= x1 && x <= x2
-    }
-    o.collide = function(ball) {
+    
+    collide(ball) {
+        const aInb = function(x, x1, x2) {
+            return x >= x1 && x <= x2
+        }
         // if (ball.y + ball.h > o.y) {
         //     if (ball.x > o.x && ball.x < o.x + o.w) {
         //         log('相撞')
@@ -36,7 +40,7 @@ const Paddle = function(game) {
         //     }
         // }
         // return false
-        const a = o
+        const a = this
         const b = ball
         if (aInb(a.x, b.x, b.x + b.w) || aInb(b.x, a.x, a.x + a.w)) {
             if (aInb(a.y, b.y, b.y + b.h) || aInb(b.y, a.y, a.y + a.h)) {
@@ -45,5 +49,56 @@ const Paddle = function(game) {
         }
         return false
     }
-    return o
+    
 }
+//
+// const Paddle = function(game) {
+//     const o = game.imageByName('paddle')
+//     // const o = {
+//     //     image: image,
+//     //     x: 100,
+//     //     y: 250,
+//     //     speed: 15,
+//     // }
+//     o.x = 100
+//     o.y = 250
+//     o.speed = 15
+//     const paddle = o
+//     o.move = function(x) {
+//         if (x < 0) {
+//             x = 0
+//         }
+//         if (x > 400 - o.w) {
+//             x = 400 - o.w
+//         }
+//         o.x = x
+//     }
+//     o.moveLeft = function() {
+//         o.move(paddle.x - paddle.speed)
+//     }
+//
+//     o.moveRight = function() {
+//         o.move(paddle.x + paddle.speed)
+//     }
+//     const aInb = function(x, x1, x2) {
+//         return x >= x1 && x <= x2
+//     }
+//     o.collide = function(ball) {
+//         // if (ball.y + ball.h > o.y) {
+//         //     if (ball.x > o.x && ball.x < o.x + o.w) {
+//         //         log('相撞')
+//         //         return true
+//         //     }
+//         // }
+//         // return false
+//         const a = o
+//         const b = ball
+//         if (aInb(a.x, b.x, b.x + b.w) || aInb(b.x, a.x, a.x + a.w)) {
+//             if (aInb(a.y, b.y, b.y + b.h) || aInb(b.y, a.y, a.y + a.h)) {
+//                 return true
+//             }
+//         }
+//         return false
+//     }
+//     return o
+// }
